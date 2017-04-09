0984a9557d252000be01ac41537525c2de238ec2
khvilaboa <khvilaboa@esei.uvigo.es>
2017-04-09 14:05:06 +0200

    Added structs to manage lines data

--- irrigation/irrigation.ino
+++ irrigation/irrigation.ino
@@ -3,16 +3,36 @@
 const int SENSOR_TEMP_START_PIN = A0; // NUM_SENSORS from here (temp)
 const int IRRIGATION_START_PIN = 2; // NUM_SENSORS irrigations from here
 
-const int NUM_SENSORS = 2;
+//const int NUM_SENSORS = 2;
 const int NUM_LINES = 2;
 const int REFRESH_TIME = 2000;
 
 // ----------------------------
 
 // Each line has NUM_SENSORS sensors (temp, humidity...)
-float startThresholds[NUM_LINES][NUM_SENSORS] = {{22.5, 0}, {22.5, 0}};
-float stopThresholds[NUM_LINES][NUM_SENSORS] = {{22, 0}, {22, 0}}; 
-bool activeLines[2]; 
+//float startThresholds[NUM_LINES][NUM_SENSORS] = {{22.5, 0}, {22.5, 0}};
+//float stopThresholds[NUM_LINES][NUM_SENSORS] = {{22, 0}, {22, 0}};
+
+struct irrigationLine {
+  bool configured;
+  char name[12];
+  int maxIrrSeconds;  // Maximum duration of a irrigation
+  int minIntSeconds;  // Minimum interval between irrigations
+
+  // Temperature
+  int tempStartOp;      // Operator
+  float tempStartThr;   // Threshold
+  int tempStopOp;
+  float tempStopThr;
+
+  // Humidity
+  int humStartOp;
+  float humStartThr;
+  int humStopOp;
+  float humStopThr;
+} lines[NUM_LINES];
+
+
 
 // ----------------------------
 
@@ -29,19 +49,31 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   
-  for(int sensorOffset=0; sensorOffset < NUM_LINES; sensorOffset++) {
-    pinMode(IRRIGATION_START_PIN + sensorOffset, OUTPUT);
-    digitalWrite(IRRIGATION_START_PIN + sensorOffset, LOW);
+  for(int numSensor=0; numSensor < NUM_LINES; numSensor++) {
+    pinMode(IRRIGATION_START_PIN + numSensor, OUTPUT);
+    digitalWrite(IRRIGATION_START_PIN + numSensor, LOW);
   }
+
+  // Fixed config for test 
+  lines[0].configured = true;
+  lines[0].tempStartThr = 22;
+  lines[0].tempStopThr = 22;
+
+  lines[1].configured = true;
+  lines[1].tempStartThr = 23;
+  lines[1].tempStopThr = 22;
+  
+  for(int numSensor=2; numSensor < NUM_LINES; numSensor++)
+    lines[numSensor].configured = false;
 }
 
 void loop() {
   //Serial.print();
-  for(int sensorOffset=0; sensorOffset < NUM_LINES; sensorOffset++) {
-    Serial.println("Sensor " + (String) sensorOffset + ": " + 
-                               (String) readTemp(SENSOR_TEMP_START_PIN + sensorOffset) + " C " + 
-                               "(sTh: " + (String) startThresholds[sensorOffset][0] + ") -> " + 
-                               (String) digitalRead(IRRIGATION_START_PIN + sensorOffset));
+  for(int numSensor=0; numSensor < NUM_LINES; numSensor++) {
+    Serial.println("Sensor " + (String) numSensor + ": " + 
+                               (String) readTemp(SENSOR_TEMP_START_PIN + numSensor) + " C " + 
+                               "(sTh: " + (String) lines[numSensor].tempStartThr + " / " + (String) lines[numSensor].tempStopThr + ") -> " + 
+                               (String) digitalRead(IRRIGATION_START_PIN + numSensor));
   }
   updateLines();
   
@@ -62,12 +94,20 @@ float readTemp(int sensor) {
 
 // Check if the lines accomplish the conditions to start/stop
 void updateLines() {
-  // Check start conditions
-  for(int sensorOffset=0; sensorOffset < NUM_LINES; sensorOffset++) {
-    if(readTemp(SENSOR_TEMP_START_PIN + sensorOffset) > startThresholds[sensorOffset][0]) {  // Start condition
-      digitalWrite(IRRIGATION_START_PIN + sensorOffset, HIGH);
-    } else if(readTemp(SENSOR_TEMP_START_PIN + sensorOffset) < stopThresholds[sensorOffset][0]) { // Stop condition
-      digitalWrite(IRRIGATION_START_PIN + sensorOffset, LOW);
+  float currentTemp;
+  
+  // Check conditions
+  for(int numSensor=0; numSensor < NUM_LINES; numSensor++) {
+    if(!lines[numSensor].configured) break;
+    Serial.println("Checking line " + (String) numSensor + "...");
+
+    currentTemp = readTemp(SENSOR_TEMP_START_PIN + numSensor);
+    
+    if(currentTemp > lines[numSensor].tempStartThr) {  // Start condition
+      digitalWrite(IRRIGATION_START_PIN + numSensor, HIGH);
+      
+    } else if(currentTemp < lines[numSensor].tempStopThr) { // Stop condition
+      digitalWrite(IRRIGATION_START_PIN + numSensor, LOW);
     }
   }
   
